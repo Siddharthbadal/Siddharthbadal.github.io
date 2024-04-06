3d3661de5b5f78a2ec6f2a392566be13f8f808a4
siddharthbadal <siddharthabadal.2015@gmail.com>
2024-04-06 21:51:33 +0530

    'changes'

--- projects.js
+++ projects.js
@@ -2,10 +2,10 @@ export const projects=[
 
     {   
         'image': 'images/automate.png',
-        'title': 'Data handling - Django Automation',
+        'title': 'Data Automation ToolKit',
         'techs': ['tech1','tech2','tech3'],
         'tech': 'Django',
-        'about': 'Developed with django, celery, redis and docker. Importing and exporting data with custom commands and UI. sending notifications to client. ',
+        'about': 'Developed with django, celery, redis and docker. Data automation toolkil allows user to export and import data with sending emails and imag compression.',
         'github':"https://github.com/Siddharthbadal/djangoAutomationProject"
     },
     {   
@@ -13,7 +13,7 @@ export const projects=[
         'title': 'Human Resource Management System',
         'techs': ['tech1','tech2','tech3'],
         'tech': 'Django',
-        'about': 'Full Stack project developed with django and bootstrap. Human Resource Management job portal to manage applications and candidates details with User registrations, forms and user validations, Custom Admin Panel, one to one chat and messages.',
+        'about': 'Full Stack project developed with django and bootstrap, with messaging, validations, authentication.',
         'github':"https://github.com/Siddharthbadal/Course-Registrar"
     },
     {   
@@ -21,7 +21,7 @@ export const projects=[
         'title': 'Command Line Interface App',
         'techs': ['tech1','tech2','tech3','tech4'],
         'tech': 'Python',
-        'about': 'Command Line Interface for Course Registrar is a project built with python, mysql and typer where users can manage their students and courses data. Users can perform various operations like create, read, update, and delete on data. users start with setting up a database either by initializing the inbuilt dataset or can add his/her data.',
+        'about': 'Command Line Interface for Course Registrar is built with python, mysql and typer where users can read, update, delete and create entries through defined commands and inputs. ',
         'github':"https://github.com/Siddharthbadal"
     },
     {   
@@ -29,7 +29,7 @@ export const projects=[
         'title': 'Social Media Platform',
         'techs': ['tech1','tech2','tech3'],
         'tech': 'Django',
-        'about': 'Built with django, bootstrap and javascript, dconnect platform is a social media project with user authentications, email settings. Users can create profile, update projects, start a study group, join a group and comment on different projects. ',
+        'about': 'Developed with django, bootstrap and javascript, It is a social media project with user authentications, user profiles and study groups. ',
         'github':"https://github.com/Siddharthbadal/DConnect-Social"
     },
     {   
@@ -42,18 +42,18 @@ export const projects=[
     },
     {   
         'image': 'images/sqlcasestudyTwo.png',
-        'title': 'SQL Business Case Studies and Data Analysis',
+        'title': 'SQL Business Case Studies',
         'techs': ['tech1','tech2','tech3'],
         'tech': 'SQL',
-        'about': 'Complete case study on adventureworks2019 database. Writing complex SQL queries using joins, grouping, aggregation, nested subqueries, windows functions, CTEs. This project includes over 50+ queries answering almost all possible data points about a business case study. ',
+        'about': 'Complete business case studies and data analysis on adventureworks2019 database, using joins, grouping, subqueries, windows functions, CTEs. ',
         'github':"https://github.com/Siddharthbadal/Data-Analysis-AdventureWorks2019"
     },
     {   
         'image': 'images/youtubeAPI.png',
-        'title': 'Youtube Channels Data Analysis with API',
+        'title': 'Youtube Data Analysis with API',
         'techs': ['tech1','tech2','tech3'],
         'tech': 'Python',
-        'about': 'Fetching data for multiple channels and analyzing Free Code Camp Youtube channel through Youtube API. Data analysis is performed using Python, Pandas, matplotlib and seaborn on Jupyter Notebook.',
+        'about': 'Fetching videos data for multiple channels and analyzing Free Code Camp Youtube channel through Youtube API. Used Pandas, matplotlib and seaborn on Jupyter Notebook for anaylsis.',
         'github':"https://github.com/Siddharthbadal/YouTubeAPI-DataAnalysis/tree/main"
     },
     {   
